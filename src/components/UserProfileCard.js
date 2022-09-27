40fddaa2ab27c3a670cf4dc02d9d2473018d761a
EmmanuelOdedele <emmaodedele@gmail.com>
2022-09-27 13:36:16 +0100

    firebase

--- src/components/UserProfileCard.js
+++ src/components/UserProfileCard.js
@@ -1,11 +1,26 @@
-import React from "react";
-import Avatar from "./Avatar"
+import React, { Suspense } from "react";
+// import Avatar from "./Avatar"
 import "../assets/styles/card.css"
+
+// Lazy load the Avatar Component
+const Avatar = React.lazy(()=>import("./Avatar"))
 
 function UserProfileCard() {
   return (
+    // normal div below
+    // <div className="card">
+    //   <Avatar />
+    //   <div className="card-name">Emmanuel Odedele</div>
+    //   <div className="card-bio">
+    //     Emmanuel Odedele will become the president of Nigeria.
+    //   </div>
+    // </div>
+
+    //new div to practise lazy loading on Avatar
     <div className="card">
-      <Avatar />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Avatar/>
+      </Suspense>
       <div className="card-name">Emmanuel Odedele</div>
       <div className="card-bio">
         Emmanuel Odedele will become the president of Nigeria.
---
+++ src/config/index.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCOueCoB8-RQ_uyrIrGqCU7aNiddqSTPNc",
+  authDomain: "emmanuel-my-app.firebaseapp.com",
+  projectId: "emmanuel-my-app",
+  storageBucket: "emmanuel-my-app.appspot.com",
+  messagingSenderId: "454674279388",
+  appId: "1:454674279388:web:9d6d2c527e14c004eebd19",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
